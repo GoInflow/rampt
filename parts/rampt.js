1e36f75d12a827428806ea8f8b7b304336081425
Juan Castro <info@jempe.org>
2019-11-20 10:51:56 -0500

    simplify rampt, no rampt-img tag

--- parts/rampt.js
+++ parts/rampt.js
@@ -36,59 +36,6 @@ if(window.IntersectionObserver)
 */
 function setup_rampt_imgs()
 {
-	var rampt_imgs = document.querySelectorAll("rampt-img:not(.rampt_observed)");
-
-	for(var rampt_img_index = 0; rampt_img_index < rampt_imgs.length; rampt_img_index++)
-	{
-		var rampt_img = rampt_imgs[rampt_img_index];
-		rampt_img.classList.add("rampt_observed");
-		if(rampt_img.getAttribute("rampt_resized") != "true")
-		{
-			rampt_img.setAttribute("rampt_resized", "true");
-			rampt_img.classList.add("i-rampthtml-layout-size-defined");
-			rampt_img.classList.add("i-rampthtml-element");
-
-			var rampt_layout = rampt_img.getAttribute("layout");
-
-			if(rampt_layout == "responsive")
-			{
-				rampt_img.classList.add("i-rampthtml-layout-responsive");
-			}
-			else if(rampt_layout == "fixed")
-			{
-				rampt_img.classList.add("i-rampthtml-layout-fixed");
-				rampt_img.style.width = rampt_img.getAttribute("width") + "px";
-				rampt_img.style.height = rampt_img.getAttribute("height") + "px";
-			}
-
-			var rampt_img_src_width = parseInt(rampt_img.getAttribute("width"));
-			var rampt_img_src_height = parseInt(rampt_img.getAttribute("height"));
-
-			var rampt_size_ratio = (rampt_img_src_height / rampt_img_src_width) * 100;
-
-			var rampt_sizer = document.createElement("i-rampthtml-sizer");
-			rampt_sizer.style.paddingTop = rampt_size_ratio + "%"; 
-
-			rampt_img.appendChild(rampt_sizer);
-
-			if(window.IntersectionObserver)
-			{
-				if(rampt_is_in_viewport(rampt_img))
-				{
-					load_rampt_img(rampt_img);
-				}
-				else
-				{
-					rampt_observer.observe(rampt_img);
-				}
-			}
-			else
-			{
-				load_rampt_img(rampt_img);
-			}
-		}
-	}
-
 	var rampt_imgs = document.querySelectorAll("img[data-rampt-src]:not(.rampt_observed)");
 
 	for(var rampt_img_index = 0; rampt_img_index < rampt_imgs.length; rampt_img_index++)
@@ -112,7 +59,6 @@ function setup_rampt_imgs()
 		}
 	}
 }
-
 /* checks is an element is visible and inside the viewport 
 */
 function rampt_is_in_viewport(rampt_element)
@@ -134,96 +80,30 @@ function rampt_is_in_viewport(rampt_element)
 */
 function load_rampt_img(rampt_container)
 {
-	if(rampt_container.tagName.toLowerCase() == "img")
+	if(rampt_container.src == "" || rampt_container.src.indexOf("data:image/png;base64,") === 0)
 	{
-		if(rampt_container.src == "" || rampt_container.src.indexOf("data:image/png;base64,") === 0)
-		{
-			if(rampt_container.dataset.webpsrc && rampt_support_webp())
-			{
-				rampt_container.src = rampt_container.dataset.webpsrc;
-				rampt_container.setAttribute("data-fallbacksrc", rampt_container.getAttribute("data-rampt-src"));
-			}
-			else
-			{
-				rampt_container.src = rampt_container.getAttribute("data-rampt-src");
-			}
-
-			if(rampt_container.dataset.webpsrcset && rampt_support_webp())
-			{
-				rampt_container.srcset = rampt_container.dataset.webpsrcset;
-				rampt_container.setAttribute("data-fallbacksrcset", rampt_container.dataset.webpsrcset);
-			}
-			else if(rampt_container.getAttribute("data-rampt-srcset"))
-			{
-				rampt_container.srcset = rampt_container.getAttribute("data-rampt-srcset");
-			}
-		}	
-	}
-	else if(rampt_container.querySelectorAll("img").length == 0)
-	{
-		rampt_container.classList.add("rampt-loading");
-		var rampt_img = document.createElement("img");
-
 		if(rampt_container.dataset.webpsrc && rampt_support_webp())
 		{
-			rampt_img.src = rampt_container.dataset.webpsrc;
-			rampt_img.setAttribute("data-fallbacksrc", rampt_container.getAttribute("src"));
+			rampt_container.src = rampt_container.dataset.webpsrc;
+			rampt_container.setAttribute("data-fallbacksrc", rampt_container.getAttribute("data-rampt-src"));
 		}
 		else
 		{
-			rampt_img.src = rampt_container.getAttribute("src");
+			rampt_container.src = rampt_container.getAttribute("data-rampt-src");
 		}
-
-		rampt_img.alt = rampt_container.getAttribute("alt");
 
 		if(rampt_container.dataset.webpsrcset && rampt_support_webp())
 		{
-			rampt_srcset = rampt_container.dataset.webpsrcset;
-			rampt_img.setAttribute("data-fallbacksrcset", rampt_container.getAttribute("srcset"));
+			rampt_container.srcset = rampt_container.dataset.webpsrcset;
+			rampt_container.setAttribute("data-fallbacksrcset", rampt_container.dataset.webpsrcset);
 		}
-		else
+		else if(rampt_container.getAttribute("data-rampt-srcset"))
 		{
-			rampt_srcset = rampt_container.getAttribute("srcset");
+			rampt_container.srcset = rampt_container.getAttribute("data-rampt-srcset");
 		}
-
-		var rampt_img_width = rampt_container.clientWidth;
-
-		if(rampt_srcset != null)
-		{
-			var rampt_sizes = rampt_srcset.split(",");
-
-			var rampt_smaller_size = 1000000;
-
-			for(rampt_size_index in rampt_sizes)
-			{
-				var rampt_size = rampt_sizes[rampt_size_index];
-
-				var rampt_size_reg = /\s[0-9]+w/;
-
-				if(rampt_size_reg.test(rampt_size))
-				{
-					var rampt_src_size = parseInt(rampt_size.match(rampt_size_reg)[0].replace("w", ""));
-
-					if(rampt_src_size > rampt_img_width && rampt_src_size < rampt_smaller_size)
-					{
-						rampt_smaller_size = rampt_src_size;
-
-						rampt_img.src = rampt_size.replace(rampt_size_reg, "").replace(/^\s+/, "").replace(/\s+$/, "");
-					}
-				} 
-			}
-
-			rampt_img.srcset = rampt_srcset;
-		}
-
-		rampt_img.classList.add("i-rampthtml-fill-content");
-		rampt_img.classList.add("i-rampthtml-replaced-content");
 
 		rampt_img.setAttribute("onerror", "rampt_fallback(this);");
-
-		rampt_container.appendChild(rampt_img);
-
-	}
+	}	
 }
 function rampt_fallback(this_rampt_img)
 {
